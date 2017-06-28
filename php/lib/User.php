8a31f92055b2ccfcb1f7be2e747dd7252971a9ba
yueyongdong <yueyongdong@gmail.com>
2017-06-29 00:29:28 +0800

    add method addAnonymous and modify method updateData...

--- php/lib/User.php
+++ php/lib/User.php
@@ -41,13 +41,18 @@ class User {
 		// 跳过前面的#行
 		for ($line = fgets($fp); $line[0] == '#'; $line = fgets($fp));
 		
-		$id = self::detailStringtoArray($line)['id'];
+		// 跳过id不符合的若干行
+		for ($line = fgets($fp), $id = self::detailStringtoArray($line)['id']; 
+			$content['id'] < $id;
+			$line = fgets($fp), $id = self::detailStringtoArray($line)['id']);
 		$lineLength = strlen($line);
 
 		if ($content['id'] == $id) {
 			fseek($fp, 0 - $lineLength, SEEK_CUR);
 			$write = self::detailArraytoString($content);
 			fwrite($fp, $write) or die('write userdata file failed!');
+		} else {
+			die('seem not exist the line with id=' . $content['id']);
 		}
 		fclose($fp);
 	}
@@ -73,6 +78,26 @@ class User {
 		fwrite($fp, $write) or die('write userdata file failed!');
 		fwrite($fp, "\r\n");
 		fclose($fp);
+	}
+	
+	// 生成随机匿名帐号
+	// 看起来目前$content里只需要提供ip
+	public static function addAnonymous($file, $content) {
+		if (!isset($content['ip'])) {
+			die('the ip seems not to set...');
+		}
+		
+		// 使用kotori+当前时间戳的md5的前20位作为用户名
+		$content['username'] = substr(md5('kotori' + mktime()), 0, 20);
+		$content['password'] = sha1('kotori');
+		$content['anonymous'] = '1';
+		
+		self::addData($file, $content);
+	}
+	
+	// 检查分配SESSION
+	public static function sessionCheck() {
+		
 	}
 }
 
