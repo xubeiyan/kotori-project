5b3bbc4b39e5c8d14983e50e6f5589fce6046f03
yueyongdong <yueyongdong@gmail.com>
2017-06-28 13:21:29 +0800

    add click upload...

--- php/templates/default/js/tools.js
+++ php/templates/default/js/tools.js
@@ -1,5 +1,6 @@
 var area = document.getElementById('area'),
-	preview = document.getElementById('preview');
+	preview = document.getElementById('preview'),
+	file = document.getElementById('file');
 	
 area.addEventListener("dragleave", function(e) {
 	e.preventDefault();
@@ -14,6 +15,36 @@ area.addEventListener("dragover", function(e) {
 area.addEventListener("drop", function (e) {
 	e.preventDefault();
 	var fileList = e.dataTransfer.files;
+	
+	if (fileList.length == 0) {
+		return false;
+	}
+	
+	if (fileList[0].type.indexOf('image') == -1) {
+		console.log('拖放的不是图片...');
+		return false;
+	}
+	
+	var img = window.URL.createObjectURL(fileList[0]),
+		filename = fileList[0].name,
+		filesize = Math.floor((fileList[0].size) / 1024);
+		
+	if (filesize > 5000) {
+		console.log('文件大小不能超过5000K！');
+		return false;
+	}
+	
+	var str = '<img id="uploadImg" src="' + img + '"><p>图片名称：' + filename + '</p>' +
+			'<p>大小:' + filesize + 'KB</p>';
+	preview.innerHTML = str;
+});
+
+area.addEventListener("click", function () {
+	file.click();
+});
+
+file.addEventListener("change", function (e) {
+	var fileList = e.target.files;
 	
 	if (fileList.length == 0) {
 		return false;
