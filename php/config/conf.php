3888447e1d7e07d2de957534b97309f4deb5c3ac
xubeiyan <yueyongdong@gmail.com>
2017-08-25 13:33:03 +0800

    add some descriptions for every item...

--- php/config/conf.php
+++ php/config/conf.php
@@ -5,8 +5,8 @@
 */
 $config = Array (
 	'user' => Array(
-		'adminUserName' => 'kotori',
-		'adminPassword' => 'kotori',
+		'adminUserName' => 'kotori',		#管理员用户名
+		'adminPassword' => 'kotori',		#管理员密码
 		'allowAnonymousUpload' => true, 	#是否允许匿名传图
 		'anonymousMaxFileSize' => 4096, 	#匿名最大传图大小(单位K)
 		'userMaxFileSize' => 10240,			#用户最大传图大小(单位K)
@@ -14,10 +14,14 @@ $config = Array (
 		'userDataFile' => 'data/userdata'	#用户信息所在位置
 	),
 	'file' => Array(
-		'allowFileType' => Array(IMAGETYPE_GIF => 'gif', IMAGETYPE_JPEG => 'jpg', 
-		IMAGETYPE_PNG => 'png', IMAGETYPE_WBMP => 'webp'),
-		'uploadFolder' => 'uploads',
-		'thumbFolder' => 'thumbs',
+		'allowFileType' => Array(			#允许文件类型
+			IMAGETYPE_GIF => 'gif', 
+			IMAGETYPE_JPEG => 'jpg', 
+			IMAGETYPE_PNG => 'png', 
+			IMAGETYPE_WBMP => 'webp'
+		),
+		'uploadFolder' => 'uploads',		#上传文件夹
+		'thumbFolder' => 'thumbs',			#略缩图文件夹
 		'r18Cover' => 'templates/default/r18cover.jpg',
 		'imageDataFile' => 'data/imagedata',#图片信息所在位置
 		'imagePerPage' => 10,
