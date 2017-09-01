fd99aafdd94d0704e0e230ff6e2f26266ba58578
xubeiyan <yueyongdong@gmail.com>
2017-09-01 14:49:23 +0800

    add enter key for login button...

--- php/templates/default/js/login.js
+++ php/templates/default/js/login.js
@@ -3,34 +3,41 @@ var username = document.getElementById('username'),
 	loginButton = document.getElementById('login-button'),
 	errmsg = document.getElementById('error-msg'),
 	xhr = new XMLHttpRequest(),
-	form = new FormData();
-	
-loginButton.addEventListener("click", function() {
-	var un = username.value,
-		pw = password.value;
+	form = new FormData(),
+	loginPost =	function() {
+		var un = username.value,
+			pw = password.value;
+			
+		// 检查用户名
+		if (un == '') {
+			console.log('请填写用户名');
+			errmsg.innerText = 'Please input username';
+			return;
+		}
 		
-	// 检查用户名
-	if (un == '') {
-		console.log('请填写用户名');
-		errmsg.innerText = 'Please input username';
-		return;
+		// 检查密码
+		if (pw == '') {
+			console.log('请填写密码');
+			errmsg.innerText = 'Please input password';
+			return;
+		}
+		
+		errmsg.innerText = '';
+		form.append('username', un);
+		form.append('password', pw);
+		
+		xhr.open('POST', '?loginpost', true);
+		xhr.setRequestHeader('Kotori-Request', 'Login');
+		xhr.send(form);
+	};
+	
+window.addEventListener("keypress", function (e) {
+	if (e.keyCode == 13) {
+		loginPost();
 	}
-	
-	// 检查密码
-	if (pw == '') {
-		console.log('请填写密码');
-		errmsg.innerText = 'Please input password';
-		return;
-	}
-	
-	errmsg.innerText = '';
-	form.append('username', un);
-	form.append('password', pw);
-	
-	xhr.open('POST', '?loginpost', true);
-	xhr.setRequestHeader('Kotori-Request', 'Login');
-	xhr.send(form);
 });
+	
+loginButton.addEventListener("click", loginPost);
 
 xhr.onreadystatechange = function () {
 	if (xhr.readyState == 4 && xhr.status == 200) {
