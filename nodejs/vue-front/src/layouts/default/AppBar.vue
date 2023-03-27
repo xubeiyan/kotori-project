df5252a56d4a010d705c569e53631605b09edc79
xubeiyan <yueyongdong@gmail.com>
2023-03-28 07:28:27 +0800

    vue 3.2 version

---
+++ nodejs/vue-front/src/layouts/default/AppBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon @click="$emit('clickIt')"/>
+    <v-toolbar-title>Kotori Project</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
---
+++ nodejs/vue-front/src/layouts/default/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <default-bar @click-it="toggleNavigationOpen" />
+    <drawer :navigation_open="navigation_open" />
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import DefaultBar from "./AppBar.vue";
+import Drawer from "./Drawer.vue";
+import DefaultView from "./View.vue";
+
+// 菜单是否打开
+let navigation_open = ref(true)
+
+// 菜单打开关闭切换
+const toggleNavigationOpen = () => {
+  // console.log(navigation_open.value)
+  navigation_open.value = !navigation_open.value;
+};
+</script>
